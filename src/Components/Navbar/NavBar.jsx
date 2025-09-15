88f8a89895e1ad573da36834fb1a32c547a02f82
mohamedx1 <mohamedalione11@gmail.com>
2025-09-15 17:33:31 +0300

    some edits in navbar&footer

--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-sm my-navbar p-4 mb-3 fixed-top ">
+            <nav className="navbar navbar-expand-sm my-navbar p-4 mb-2 fixed-top ">
                 <div className="container">
                     <NavLink className="navbar-brand  fs-1" to="/">START FRAMEWORK</NavLink>
                     <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,11 +21,6 @@ const NavBar = () => {
                                 <NavLink className="nav-link  text-white text-uppercase fw-bold mt-3 mt-md-0 px-2  rounded-3" to="/portfolio" aria-current="page">Portfolio
                                     <span className="visually-hidden">(current)</span></NavLink>
                             </li>
-                            <li className="nav-item mx-3">
-                                <NavLink className="nav-link  text-white text-uppercase fw-bold mt-3 mt-md-0 px-2  rounded-3" to="/Contact" aria-current="page">Contact
-                                    <span className="visually-hidden">(current)</span></NavLink>
-                            </li>
-
                         </ul>
                     </div>
                 </div>
